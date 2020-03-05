448081740277c1303552f1e9138639b8fb0ebf49
Mykola Mokhnach <mokhnach@gmail.com>
2020-03-05 07:27:42 +0100

    feat: Introduce continuous integration (#3)

---
+++ README.md
@@ -0,0 +1,19 @@
+### unsign.jar
+
+Deletes META-INF folder from a .zip/.apk file:
+
+`$ java -jar unsign.jar test.apk`
+
+### move_manifest.jar
+
+Moves a new Android manifest file into a .zip/.apk file:
+
+`$ java -jar move_manifest.jar test.apk AndroidManifest.xml`
+
+### Build
+
+`$ ./build.sh`
+
+### Test
+
+`$ ./test.sh`
---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ex
+
+version=0.0.1-SNAPSHOT
+if [[ -n "$1" ]]; then
+  version=$1
+fi
+
+awk "NR==1,/<version>.+<\/version>/{sub(/<version>.+<\/version>/, \"<version>$version<\/version>\")} 1" pom.xml > pom_new.xml
+mv -f pom_new.xml pom.xml
+mvn clean
+mvn package
+mvn -DmainClass=unsign.MoveManifest -DnamePrefix=move_manifest package
